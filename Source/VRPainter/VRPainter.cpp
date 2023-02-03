dd14381082039441d7bf7895fd351eb72c88d672
aleksavr84 <aleksavr84@gmail.com>
2023-02-03 15:33:43 +0100

    First Init

---
+++ Source/VRPainter/VRPainter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VRPainter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VRPainter, "VRPainter" );
---
+++ Source/VRPainter/VRPainterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRPainterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRPAINTER_API AVRPainterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
